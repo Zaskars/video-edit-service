db4322d2fd805714cdc2bd4909bdbe2773456268
astrogator <timbolotov3@gmail.com>
2024-05-15 05:49:50 +0300

    -sync download function

--- utils.py
+++ utils.py
@@ -14,19 +14,3 @@ def save_uploaded_file(file) -> str:
     return file_path
 
 
-def download_video(source: str) -> str:
-    if not os.path.exists(DOWNLOADS_DIR):
-        os.makedirs(DOWNLOADS_DIR)
-
-    ydl_opts = {'outtmpl': os.path.join(DOWNLOADS_DIR, '%(title)s.%(ext)s')}
-
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        info_dict = ydl.extract_info(source, download=True)
-        video_file = ydl.prepare_filename(info_dict)
-
-        safe_video_file = re.sub(r'[^A-Za-z0-9_\-./]', '_', video_file)
-        safe_video_file_path = os.path.join(DOWNLOADS_DIR, safe_video_file)
-
-        os.rename(video_file, safe_video_file_path)
-
-        return safe_video_file_path
